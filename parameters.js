d78745d2e34b288f868195182ef8d64eaac36be0
Ziyu Tao <ziyu.tao@outlook.com>
2021-11-04 03:59:22 +0800

    update param docs

--- parameters.js
+++ parameters.js
@@ -1,16 +1,16 @@
 //const doorRatio = 80 / 200;
-const doorRatio = 120 / 200;
-const DISPOINT_RATIO = 20;
+const doorRatio = 120 / 200; // ratio of the door, infact the line above is the standard usable door, but it may not looks good on layout
+const DISPOINT_RATIO = 20; // area of door handle
 
-const COLOR_WIDTH = 3;
-const FREE_COLOR = true;
+const COLOR_WIDTH = 3; // how many line together share the same color
+const FREE_COLOR = true; // use custom color sets, edit COLORS, with RGB string list
 //const COLOR_WIDTH = 12;
-//const FREE_COLOR = false;
-const S = 100;
-const V = 92;
+//const FREE_COLOR = false; // use HSV ring by default, the above line is width I suggest
+const S = 100; // Saturation
+const V = 92; // Brightness
 
-const WEIGHT = 4;
-const DENCE = 30;
+const WEIGHT = 4; // Line Weight
+const DENCE = 30; // Dence of lines, greater than 360 to make a surface, but I like this more
 
 const FR = 60;
 
