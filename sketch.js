2b60d83b23cf8ca67dc84b55ae8c7de04405be7c
Ziyu Tao <ziyu.tao@outlook.com>
2021-11-03 13:13:56 +0800

    initial commit

---
+++ sketch.js
@@ -0,0 +1,33 @@
+// color schemes
+const SCHEMES = {
+  red: ['#ff867c', '#ef5350', '#b61827'],
+  pink: ['#ffc1e3', '#f48fb1', '#bf5f82'],
+  purple: ['#ffc4ff', '#ce93d8', '#9c64a6'],
+  skyblue: ['#72e7ff', '#28b5f4', '#0085c1'],
+  cyan: ['#b4ffff', '#80deea', '#4bacb8'],
+  teal: ['#b2fef7', '#80cbc4', '#4f9a94'],
+  green: ['#98ee99', '#66bb6a', '#338a3e'],
+  lime: ['#ffffce', '#e6ee9c', '#b3bc6d'],
+  yellow: ['#ffffcf', '#fff59d', '#cbc26d'],
+  orange: ['#ffffb0', '#ffcc80', '#ca9b52'],
+  blue: ['#302F7A', '#ABAAFA', '#625FFA', '#54537A', '#4E4CC7'],
+  custom: [],
+  grave: '#e0e0e0',
+};
+const FR = 30;
+var config = {
+
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  // put setup code here
+  background(30);
+  frameRate(FR);
+  angleMode(DEGREES)
+}
+
+function draw() {
+  // put drawing code here
+}
+
